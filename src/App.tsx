332c7020400e2a0197acca54d2564058d4ac48ce
tuheli <94482031+tuheli@users.noreply.github.com>
2024-09-17 15:41:50 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { DefaultAppBar } from "./appbar/DefaultAppBar";
+
+export const App = () => {
+  return (
+    <>
+      <DefaultAppBar />
+    </>
+  );
+};
---
+++ src/historical-figures/WelcomeAIHistoricalFigures.tsx
@@ -0,0 +1,3 @@
+export const WelcomeAIHistoricalFigures = () => {
+  return null;
+};
