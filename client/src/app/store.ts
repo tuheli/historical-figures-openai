7f30fc8fe8b7ed9b82896946c54fe5d0f11ca0d0
tuheli <94482031+tuheli@users.noreply.github.com>
2024-09-21 00:04:32 +0300

    Added api slice to store

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import historicalFigureSlice from "../features/historicalFigureSlice";
+import { apiSlice } from "../features/apiSlice";
 
 export const store = configureStore({
   reducer: {
     historicalFigure: historicalFigureSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
   },
 });
 
