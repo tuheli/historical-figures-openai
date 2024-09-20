53424aadaea584fd1febad3b977a01e06b7c53ee
tuheli <94482031+tuheli@users.noreply.github.com>
2024-09-20 22:59:25 +0300

    Update ChatWithHistoricalFigurePage.tsx

--- client/src/components/chat-with-historical-figure/ChatWithHistoricalFigurePage.tsx
+++ client/src/components/chat-with-historical-figure/ChatWithHistoricalFigurePage.tsx
@@ -1,19 +1,43 @@
-import { Stack } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
 import { HistoricalFigureProfile } from "./HistoricalFigureProfile";
+import { MessagesDrawer } from "./MessagesDrawer";
 import { Messages } from "./Messages";
+import { SendMessage } from "./SendMessage";
 
 export const ChatWithHistoricalFigurePage = () => {
   return (
     <>
-      <Stack
-        sx={{
-          flexDirection: "row",
-          m: 2,
-        }}
-      >
-        <Messages />
-        <HistoricalFigureProfile />
-      </Stack>
+      <Container maxWidth="xl">
+        <Stack
+          sx={{
+            flexDirection: "row",
+            m: 2,
+          }}
+        >
+          <MessagesDrawer />
+          <Stack
+            sx={{
+              flex: 1,
+            }}
+          >
+            <HistoricalFigureProfile />
+            <Box
+              sx={{
+                mt: 4,
+              }}
+            >
+              <Messages />
+            </Box>
+            <Box
+              sx={{
+                mt: 4,
+              }}
+            >
+              <SendMessage />
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
     </>
   );
 };
