6f6be2912926ada0289eba9d0594bde6d55e427b
tuheli <94482031+tuheli@users.noreply.github.com>
2024-09-20 21:09:47 +0300

    Initialized server

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const port = 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/", async (req, res) => {
+  return res.status(200).json({ message: "PingPong" });
+});
+
+const start = () => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+start();
