be02a746b9bf0b9c41bdb6eda643b105dbd03fcf
tuheli <94482031+tuheli@users.noreply.github.com>
2024-09-21 00:04:49 +0300

    Questions can be asked from abraham lincoln endpoint

--- client/src/components/chat-with-historical-figure/SendMessage.tsx
+++ client/src/components/chat-with-historical-figure/SendMessage.tsx
@@ -3,14 +3,17 @@ import { useAppDispatch } from "../../app/hooks";
 import { FormEvent, useState } from "react";
 import { sentMessage } from "../../features/historicalFigureSlice";
 import { useHistoricalFigure } from "../../hooks/useHistoricalFigure";
+import { useAbrahamLincolnMutation } from "../../features/apiSlice";
 
 export const SendMessage = () => {
   const [input, setInput] = useState("");
+  const [askAbrahamLincoln] = useAbrahamLincolnMutation();
   const figure = useHistoricalFigure();
   const dispatch = useAppDispatch();
 
-  const onSubmit = (event: FormEvent) => {
+  const onSubmit = async (event: FormEvent) => {
     event.preventDefault();
+
     if (!figure) return;
 
     dispatch(
@@ -25,6 +28,23 @@ export const SendMessage = () => {
     );
 
     setInput("");
+
+    try {
+      const response = await askAbrahamLincoln({ input }).unwrap();
+
+      dispatch(
+        sentMessage({
+          id: figure.id,
+          message: {
+            content: response.message,
+            date: new Date().toLocaleString(),
+            sender: figure.id,
+          },
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
