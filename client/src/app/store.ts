2a1c6b47feceb149ecdd60fcf75a460fd638aabc
tuheli <94482031+tuheli@users.noreply.github.com>
2024-09-22 15:40:35 +0300

    Added mobile slice

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import historicalFigureSlice from "../features/historicalFigureSlice";
 import { apiSlice } from "../features/apiSlice";
+import mobileSlice from "../features/mobileSlice";
 
 export const store = configureStore({
   reducer: {
     historicalFigure: historicalFigureSlice,
+    mobile: mobileSlice,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
